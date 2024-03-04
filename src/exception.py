520f430220adce17270efd17500354ae460d119e
Mohithingonia <hingonia.1@iitj.ac.in>
2024-03-05 00:04:44 +0530

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,14 +1,14 @@
 import sys
-
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb= error_detail.exc_info()
+    _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
-    error_messaged="Error occured in python script name[{0}] line number[{1}] error message[{2}]".format(
-        file_name, exc_tb.tb_lineno,str(error)
-    return error_massage
-        
-    )
+    error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
+    file_name,exc_tb.tb_lineno,str(error))
+    
+
+    return error_message
 class customException(Exception):
     def __init__(self,error_message,error_details:sys):
         super().__init__(error_message)
